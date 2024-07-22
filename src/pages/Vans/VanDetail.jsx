bb264efced020beaa18183610f11588ca5f40ca4
Ярослав Гомон <pozzzitivu@mail.ru>
2024-07-22 23:49:00 +0400

    feat: add vans filter

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link, useLocation } from 'react-router-dom';
 
 export default function VanDetail() {
   const params = useParams();
   const [van, setVan] = useState(null);
+  const location = useLocation();
 
   useEffect(() => {
     fetch(`/api/vans/${params.id}`)
@@ -11,8 +12,17 @@ export default function VanDetail() {
       .then(data => setVan(data.vans));
   }, [params.id]);
 
+  const search = location.state?.search || '';
+  const typeFilter = location.state?.type || 'all';
+
+  console.log(search);
+
   return (
     <div className='van-detail-container'>
+      <Link to={`..${search}`} relative='path' className='back-button'>
+        &larr; <span>Back to {typeFilter} vans</span>
+      </Link>
+
       {van ? (
         <div className='van-detail'>
           <img src={van.imageUrl} />
--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,21 +1,34 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 export default function Vans() {
   const [data, setData] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  let typeFilter = searchParams.get('type');
+
+  function handleClick(param) {
+    if (param) {
+      setSearchParams({ type: param });
+    } else {
+      setSearchParams({});
+    }
+  }
 
   useEffect(() => {
     fetch('/api/vans')
       .then(res => res.json())
-      // .then(data => console.log(data));
       .then(data => setData(data.vans));
   }, []);
 
-  const vansList = data.map(van => {
+  const displayedVans = typeFilter ? data.filter(val => val.type === typeFilter) : data;
+
+  const vansList = displayedVans.map(van => {
     return (
       <li key={van.id} className='van-tile'>
         <Link
-          to={`/vans/${van.id}`}
+          to={van.id}
+          state={{ search: `?${searchParams.toString()}`, type: typeFilter }}
           aria-label={`View details for ${van.name}, priced at $${van.price} per day`}
         >
           <img src={van.imageUrl} alt={`Image of ${van.name}`} />
@@ -35,6 +48,31 @@ export default function Vans() {
   return (
     <div className='van-list-container'>
       <h1>Explore our van options</h1>
+      <div className='van-list-filter-buttons'>
+        <button
+          onClick={() => handleClick('simple')}
+          className={`van-type simple${typeFilter === 'simple' ? ' selected' : ''}`}
+        >
+          Simple
+        </button>
+        <button
+          onClick={() => handleClick('luxury')}
+          className={`van-type luxury${typeFilter === 'luxury' ? ' selected' : ''}`}
+        >
+          Luxury
+        </button>
+        <button
+          onClick={() => handleClick('rugged')}
+          className={`van-type rugged${typeFilter === 'rugged' ? ' selected' : ''}`}
+        >
+          Rugged
+        </button>
+        {typeFilter && (
+          <button onClick={() => handleClick(null)} className='van-type clear-filters'>
+            Clear filters
+          </button>
+        )}
+      </div>
       <ul className='van-list'>{vansList}</ul>
     </div>
   );
